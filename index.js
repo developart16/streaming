b4b973384a4083d3cd486b0c87f8b87d91f3b624
developart16 <artur.develop@gmail.com>
2022-10-15 18:33:40 +0200

    experiment with audio streaming finished

---
+++ index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const app = require('express')();
+// const app = express();
+const PORT = 3003;
+
+app.get('/', (req, res)=>{
+    return res.status(200).send("Welcome to Artur's Streaming")
+})
+
+app.get('/music/:song', (req, res)=>{
+
+    const { song } = req.params
+    if ( !song ) return res.status(400).send("Someting went wrong with the request");
+
+    const filePath = `./music/${song}`
+    if( !fs.existsSync(filePath) ) return res.status(404).send("Music not found")
+    
+    const { range = "bytes=0-"} = req.headers;
+    
+    const fileSize = fs.statSync(filePath).size
+    
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, fileSize - 1);
+    const contentLength = end - start + 1
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "audio/mp3"
+    }
+
+    res.writeHead(206, headers)
+    const audioStream = fs.createReadStream(filePath, {start, end})
+    return audioStream.pipe(res)
+})
+
+app.listen(PORT, ()=>{
+    console.log( `✅ Express: ${PORT}` );
+})
